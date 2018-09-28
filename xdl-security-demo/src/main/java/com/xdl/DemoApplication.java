78e7763b697e4ccca708beb6d04ec76bf0b386c2
XiaoDonglu <xiaodonglu1024@163.com>
2018-09-28 11:30:53 +0800

    Initial commit

---
+++ xdl-security-demo/src/main/java/com/xdl/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.xdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * demo启动类
+ *
+ * @author xdl
+ * @date 2018-09-28
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello Spring security";
+    }
+
+}
